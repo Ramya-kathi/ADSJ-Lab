73423d20f820977c8c094891cf4d684fccb44275
Ramya kathi <142296740+Ramya-kathi@users.noreply.github.com>
2023-11-21 22:09:12 +0530

    Update Linear_Search.java

--- Linear_Search.java
+++ Linear_Search.java
@@ -13,6 +13,7 @@ public class Main
     }
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
+		System.out.println("enter the size of the array");
 	    int n = sc.nextInt();
 	    int []a = new int[n];
 	    for ( int i = 0; i < n ; i++ )
